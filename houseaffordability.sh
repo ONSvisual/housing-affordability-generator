211648467d4558f49befc196eecdeae9c606edfa
fryford <robertmarkfry@yahoo.co.uk>
2019-12-12 10:05:33 +0000

    Tweak tile generation.
    Some large tiles now....but seems OK.

--- houseaffordability.sh
+++ houseaffordability.sh
@@ -28,18 +28,16 @@ ogr2ogr -f GeoJSON -lco COORDINATE_PRECISION=5 houseprices.geojson joined.shp
 
 echo 'generating tiles'
 #Make the tiles
-#Zoom level 4 to 8
-tippecanoe --minimum-zoom=4 --maximum-zoom=9 --output-to-directory z4-8 --full-detail=9 --drop-smallest-as-needed --extend-zooms-if-still-dropping houseprices.geojson
-#Zoom level 9
-tippecanoe --minimum-zoom=9 --maximum-zoom=9 --output-to-directory z9 --full-detail=11 --no-tile-size-limit houseprices.geojson
+#Zoom level 4 to 9
+tippecanoe --minimum-zoom=4 --maximum-zoom=9 --full-detail=11 --low-detail=11 --output-to-directory z4-9 --no-tile-size-limit houseprices.geojson
 #Zoom 10 to 13
-tippecanoe --minimum-zoom=10 --maximum-zoom=13 --output-to-directory z10-13 --no-tile-size-limit --extend-zooms-if-still-dropping houseprices.geojson
+tippecanoe --minimum-zoom=10 --maximum-zoom=13 --low-detail=10 --output-to-directory z10-13 --no-tile-size-limit houseprices.geojson
+
 
 
 #move the files together
 mkdir tiles
-cp -r z4-8/ tiles
-cp -r z9/ tiles
+cp -r z4-9/ tiles
 cp -r z10-13/ tiles
 
 echo 'downloading MSOA boundaries'
@@ -52,10 +50,10 @@ mv 29fdaa2efced40378ce8173b411aeb0e_2.geojson msoaboundaries.geojson
 
 echo 'joining house prices data to MSOA boundaries'
 #drop fields we don't need
-ogr2ogr -f geojson -t_srs crs:84 -sql "SELECT msoa11cd, msoa11nm FROM fdaa2efced40378ce8173b411aeb0e_2" bounds.geojson fdaa2efced40378ce8173b411aeb0e_2.geojson
+ogr2ogr -f geojson -t_srs crs:84 -sql "SELECT msoa11cd, msoa11nm FROM msoaboundaries" bounds.geojson msoaboundaries.geojson
 
 #join house prices to boundaries too
-mapshaper-xl bounds.geojson -join houseprice.csv keys=msoa11cd,MSOAcode field-types=MSOAcode:str,	MSOAname:str,	LAcode:str,	LAname:str,	Regioncode:str,	Regionname:str,	income:num,	a_median:num,	d_median:num,	s_median:num,	t_median:num,	f_median:num,	a_lowerquartile:num,	d_lowerquartile:num,	s_lowerquartile:num,	t_lowerquartile:num,	f_lowerquartile:num,	a_tenp:num,	d_tenp:num,	s_tenp:num,	t_tenp:num,	f_tenp:num -o boundar.geojson
+mapshaper-xl bounds.geojson -join houseprices.csv keys=msoa11cd,MSOAcode field-types=MSOAcode:str,	MSOAname:str,	LAcode:str,	LAname:str,	Regioncode:str,	Regionname:str,	income:num,	a_median:num,	d_median:num,	s_median:num,	t_median:num,	f_median:num,	a_lowerquartile:num,	d_lowerquartile:num,	s_lowerquartile:num,	t_lowerquartile:num,	f_lowerquartile:num,	a_tenp:num,	d_tenp:num,	s_tenp:num,	t_tenp:num,	f_tenp:num -o boundar.geojson
 
 #drop some more fields (probably way to many here at the moment) and convert to WGS84 projection
 ogr2ogr -f geojson -t_srs crs:84 -sql "SELECT msoa11cd, msoa11nm, LAcode,	LAname,	Regioncode,	Regionname,	income,	a_median,	d_median,	s_median,	t_median,	f_median,	a_lowerquartile,	d_lowerquartile,	s_lowerquartile,	t_lowerquartile,	f_lowerquartile,	a_tenp,	d_tenp,	s_tenp,	t_tenp,	f_tenp FROM boundar" boundaries.geojson boundar.geojson
